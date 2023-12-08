a47c219c4270035cd747b5852b9b9ef52f651854
Frank Willian <frank19valentim@gmail.com>
2023-12-08 10:24:43 -0300

    Adiciona programa de Calculadora de Médias de Alunos

--- app.cjs
+++ app.cjs
@@ -1,79 +1,92 @@
+// Importando a biblioteca readline para interação com o console
 const readline = require('readline');
 
+// Criando uma interface readline para leitura e escrita no console
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const students = {};
+// Criando um objeto vazio para armazenar as notas dos alunos
+const alunos = {};
 
 // Função para coletar nome e notas de um aluno
-function collectStudentData(index, callback) {
-  rl.question(`Digite o nome do aluno ${index + 1}: `, (name) => {
-    const grades = [];
+function coletarDadosDoAluno(indice, retornoDeChamada) {
+  // Perguntando ao usuário o nome do aluno
+  rl.question(`Digite o nome do aluno ${indice + 1}: `, (nome) => {
+    const notas = [];
 
-    // Função para coletar cada nota de forma síncrona
-    function collectGrade(gradeIndex) {
-      if (gradeIndex < 3) {
-        rl.question(`Digite a nota ${gradeIndex + 1} para ${name}: `, (grade) => {
-          // Verificamos se a entrada é um número válido
-          if (!isNaN(grade)) {
-            grades.push(parseFloat(grade));
-            collectGrade(gradeIndex + 1);
+    // Função interna para coletar cada nota de forma síncrona
+    function coletarNota(indiceNota) {
+      if (indiceNota < 3) {
+        // Perguntando ao usuário cada nota
+        rl.question(`Digite a nota ${indiceNota + 1} para ${nome}: `, (nota) => {
+          // Verificando se a entrada é um número válido
+          if (!isNaN(nota)) {
+            notas.push(parseFloat(nota));
+            coletarNota(indiceNota + 1);
           } else {
+            // Se a entrada não for válida, solicitamos novamente a mesma nota
             console.log('Por favor, digite um número válido.');
-            collectGrade(gradeIndex); // Pedimos novamente a mesma nota
+            coletarNota(indiceNota);
           }
         });
       } else {
-        // Quando todas as notas são coletadas, armazenamos no objeto students
-        students[name] = grades;
-        callback(); // Chamamos a função de retorno de chamada para continuar o fluxo
+        // Quando todas as notas são coletadas, armazenamos no objeto alunos
+        alunos[nome] = notas;
+        // Chamamos a função de retorno de chamada para continuar o fluxo
+        retornoDeChamada();
       }
     }
 
-    // Iniciamos o processo de coleta de notas chamando a função collectGrade
-    collectGrade(0);
+    // Iniciamos o processo de coleta de notas chamando a função coletarNota
+    coletarNota(0);
   });
 }
 
 // Função para calcular e exibir a média das notas de um aluno
-function calculateAndDisplayAverage(name) {
-  const grades = students[name];
+function calcularEExibirMedia(nome) {
+  const notas = alunos[nome];
 
-  if (grades) {
+  if (notas) {
     // Calculamos a média das notas
-    const average = grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
-    console.log(`A média de notas para ${name} é: ${average.toFixed(2)}`);
+    const media = notas.reduce((soma, nota) => soma + nota, 0) / notas.length;
+    // Exibimos a média das notas no console
+    console.log(`A média de notas para ${nome} é: ${media.toFixed(2)}`);
   } else {
-    console.log(`Não foram encontradas notas para ${name}.`);
+    // Se não encontrarmos notas para o aluno, exibimos uma mensagem correspondente
+    console.log(`Não foram encontradas notas para ${nome}.`);
   }
 
-  rl.close(); // Fechamos a interface readline quando o processo é concluído
+  // Fechamos a interface readline quando o processo é concluído
+  rl.close();
 }
 
 // Função principal que inicia o processo de coleta de nomes e notas
-function startStudentDataCollection() {
-  let count = 0;
+function iniciarColetaDeDadosDoAluno() {
+  let contador = 0;
 
-  // Função para coletar nomes e notas para três alunos
-  function collectDataForMultipleStudents() {
-    if (count < 3) {
-      collectStudentData(count, () => {
-        count++;
-        collectDataForMultipleStudents(); // Chamamos a função de forma recursiva para o próximo aluno
+  // Função interna para coletar nomes e notas para três alunos
+  function coletarDadosParaMultiplosAlunos() {
+    if (contador < 3) {
+      // Chamamos a função para coletar dados do aluno e passamos uma função de retorno de chamada
+      coletarDadosDoAluno(contador, () => {
+        contador++;
+        // Chamamos a função de forma recursiva para o próximo aluno
+        coletarDadosParaMultiplosAlunos();
       });
     } else {
       // Quando todas as informações são coletadas, solicitamos um nome para calcular a média
-      rl.question('Digite um nome para calcular a média de notas: ', (searchName) => {
-        calculateAndDisplayAverage(searchName);
+      rl.question('Digite um nome para calcular a média de notas: ', (nomePesquisado) => {
+        // Chamamos a função para calcular e exibir a média do aluno especificado
+        calcularEExibirMedia(nomePesquisado);
       });
     }
   }
 
-  // Iniciamos o processo chamando a função collectDataForMultipleStudents
-  collectDataForMultipleStudents();
+  // Iniciamos o processo chamando a função coletarDadosParaMultiplosAlunos
+  coletarDadosParaMultiplosAlunos();
 }
 
 // Iniciamos o programa chamando a função principal
-startStudentDataCollection();
+iniciarColetaDeDadosDoAluno();
